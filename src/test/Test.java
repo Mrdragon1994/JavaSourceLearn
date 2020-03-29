185f90da55a457896a6fa9a9e93db72d7d080960
ChangQilong <694504216@qq.cm>
2020-03-29 13:28:03 +0800

    创建新项目，初始化项目

---
+++ src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author ChangQilong
+ * @Date 2020/3/29 12:59
+ */
+public class Test {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "1");
+        map.put("b", "2");
+
+    }
+}
